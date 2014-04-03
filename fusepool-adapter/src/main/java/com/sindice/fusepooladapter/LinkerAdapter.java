1cc9e67562a36a7fe214716ac14ab415fd4d49d6
Jakub Kotowski <jakubkotowski@gmx.net>
2014-04-03 17:06:50 +0100

    Fixed constructors, thx Reto

--- fusepool-adapter/src/main/java/com/sindice/fusepooladapter/LinkerAdapter.java
+++ fusepool-adapter/src/main/java/com/sindice/fusepooladapter/LinkerAdapter.java
@@ -14,10 +14,7 @@
  */
 package com.sindice.fusepooladapter;
 
-import java.io.IOException;
-import java.nio.file.FileSystems;
 import java.sql.SQLException;
-import java.util.Properties;
 
 import org.apache.clerezza.rdf.core.TripleCollection;
 import org.slf4j.Logger;
@@ -35,22 +32,6 @@ import com.sindice.fusepooladapter.storage.OutputStore;
 public class LinkerAdapter implements SingleLinker {
 
   private static final Logger logger = LoggerFactory.getLogger(LinkerAdapter.class);
-  private final String inpath;
-  private final String outpath;
-  private final String dukeConfigFileLocation;
-  private final int threadsNo;
-  
-  /**
-   * Default constructor that uses the default*() methods to initialize fields.
-   * 
-   * @throws IOException
-   */
-  public LinkerAdapter() throws IOException {
-	  this.inpath = defaultInputDir();
-	  this.outpath = defaultOutputDir();
-	  this.threadsNo = defaultNumberOfThreads();
-	  this.dukeConfigFileLocation = defaultConfigFileLocation();
-  }
   
   /**
    * Creates a default temporary directory to be used as the input dir.
@@ -96,63 +77,20 @@ public class LinkerAdapter implements SingleLinker {
 	  return "classpath:patents-jena-jdbc.xml";
   }
     
-  /**
-   * constructor using provided parameters
-   * 
-   * @param dukeConfigFileLocation - Duke XML configuraiton file path
-   * @param inpath - data path for input store
-   * @param outpath - data path for output store
-   * @param threadsNo - number of threads to run Duke
-   * @throws IOException
-   */
-  public LinkerAdapter(String dukeConfigFileLocation, String inpath, String outpath, int threadsNo)
-      throws IOException {
-    /*
-     * Properties properties = new Properties();
-     * properties.load(Thread.currentThread().getContextClassLoader()
-     * .getResourceAsStream("conf.properties"));
-     */
-    this.inpath = FileSystems.getDefault().getPath(inpath).toAbsolutePath().toString();;
-    this.outpath = FileSystems.getDefault().getPath(outpath).toAbsolutePath().toString();
-    this.dukeConfigFileLocation = dukeConfigFileLocation;
-    this.threadsNo = threadsNo;
-
-  }
-  /**
-   * constructor for configuration in a property file on classpath
-   * @param confFileName - file name of the property file
-   * @throws IOException
-   */
-  public LinkerAdapter(String confFileName)
-      throws IOException {
-
-      Properties properties = new Properties();
-      properties.load(Thread.currentThread().getContextClassLoader()
-      .getResourceAsStream(confFileName));
-      if (properties.getProperty("inpath") == null || properties.getProperty("outpath") == null ||properties.getProperty("dukeconfig") == null ||properties.getProperty("dukethrno") == null){
-        throw new IllegalArgumentException("missing mandatory property");
-      }
-      this.inpath = FileSystems.getDefault().getPath(properties.getProperty("inpath")).toAbsolutePath().toString();;
-      this.outpath = FileSystems.getDefault().getPath(properties.getProperty("outpath")).toAbsolutePath().toString();
-      this.dukeConfigFileLocation = properties.getProperty("dukeconfig");
-      this.threadsNo = Integer.parseInt(properties.getProperty("dukethrno"));
-
-  } 
-    
   @Override
   public TripleCollection interlink(TripleCollection dataToInterlink) {
     // populates input store
 	logger.info("Populating input store ...");
-    InputTripleStore instore = new JenaInputStoreImpl(inpath);
+    InputTripleStore instore = new JenaInputStoreImpl(defaultInputDir());
     int inputSize = instore.populate(dataToInterlink);
     logger.info("Input store populated with {} triples", inputSize);
-    OutputStore outStore = new OutputStore(outpath);
+    OutputStore outStore = new OutputStore(defaultOutputDir());
     outStore.clean();
     
     // starts processing
     DukeRunner runner = null;
     try {
-      runner = new DukeRunner(dukeConfigFileLocation,"jdbc:jena:tdb:location=" + inpath,  outpath, threadsNo) {
+      runner = new DukeRunner(defaultConfigFileLocation(),"jdbc:jena:tdb:location=" + defaultInputDir(),  defaultOutputDir(), defaultNumberOfThreads()) {
       //  runner = new DukeRunner(dukeConfigFileLocation,"jdbc:jena:tdb:location=" + inpath,  "jdbc:jena:tdb:location=" + outpath, threadsNo) {
       };
     } catch (SQLException e) {
